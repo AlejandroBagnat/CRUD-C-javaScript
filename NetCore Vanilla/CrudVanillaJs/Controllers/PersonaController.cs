f5bab7551d24171e1d21826a52f70898b7e3faf1
unknown <abagnat@yahoo.com.ar>
2021-05-02 21:36:04 -0300

    This is a new commit for what I originally planned to be amended

---
+++ NetCore Vanilla/CrudVanillaJs/Controllers/PersonaController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudVanillaJs.Controllers
+{
+    [EnableCors("Permitir")]
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class PersonaController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            using (Models.CrudVanillaJsContext db = new Models.CrudVanillaJsContext())
+            {
+                var lst = (from d in db.Personas
+                           select d).ToList();
+
+                return Ok(lst);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Models.Request.PersonaRequest model)
+        {
+            using (Models.CrudVanillaJsContext db = new Models.CrudVanillaJsContext())
+            {
+                Models.Persona oPersona = new Models.Persona();
+                oPersona.Nombre = model.Nombre;
+                oPersona.Edad = model.Edad;
+                db.Personas.Add(oPersona);
+                db.SaveChanges();
+            }
+            return Ok();
+        }
+
+
+        [HttpPut]
+        public ActionResult Put([FromBody] Models.Request.PersonaEditRequest model)
+        {
+            using (Models.CrudVanillaJsContext db = new Models.CrudVanillaJsContext())
+            {
+                Models.Persona oPersona = db.Personas.Find(model.Id);
+                oPersona.Nombre = model.Nombre;
+                oPersona.Edad = model.Edad;
+                db.Entry(oPersona).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                db.SaveChanges();
+            }
+            return Ok();
+        }
+
+
+        [HttpDelete]
+        public ActionResult Delete([FromBody] Models.Request.PersonaEditRequest model)
+        {
+            using (Models.CrudVanillaJsContext db = new Models.CrudVanillaJsContext())
+            {
+                Models.Persona oPersona = db.Personas.Find(model.Id);
+                db.Personas.Remove(oPersona);
+                db.SaveChanges();
+            }
+            return Ok();
+        }
+    }
+}
---
+++ NetCore Vanilla/CrudVanillaJs/Controllers/WeatherForecastController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudVanillaJs.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            using (Models.CrudVanillaJsContext db = new Models.CrudVanillaJsContext())
+            {
+                var lst = (from d in db.Personas
+                           select d).ToList();
+
+                return Ok(lst);
+            }
+
+        }
+    }
+}
